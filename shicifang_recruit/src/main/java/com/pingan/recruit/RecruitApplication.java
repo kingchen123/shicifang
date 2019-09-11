c9c9c556c3fe4bdbd0450197cf56fa508ef56f7e
itcast <bos@itcast.cn>
2019-09-11 22:57:31 +0800

    shicifang第6次提交代码

---
+++ shicifang_recruit/src/main/java/com/pingan/recruit/RecruitApplication.java
@@ -0,0 +1,19 @@
+package com.pingan.recruit;
+
+import com.pingan.util.IdWorker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+@SpringBootApplication
+public class RecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitApplication.class, args);
+	}
+
+	@Bean
+	public IdWorker idWorkker(){
+		return new IdWorker(1, 1);
+	}
+	
+}
